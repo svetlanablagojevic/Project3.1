5a5d83f67487a9a5ebbf874855e9d20f1c65ae3f
svetlanablagojevic <svetlana.sege@gmail.com>
2016-12-20 10:14:33 +0100

    Delete AnswerAcceptanceException.java

--- techdegree-javaweb-unit-testing-0.0.3/src/main/java/com/teamtreehouse/techdegree/overboard/exc/AnswerAcceptanceException.java
+++
@@ -1,8 +0,0 @@
-package com.teamtreehouse.techdegree.overboard.exc;
-
-
-public class AnswerAcceptanceException extends RuntimeException {
-    public AnswerAcceptanceException(String message) {
-        super(message);
-    }
-}
