0c603b62278bdc4fabc019fb9a828b1cb7584601
svetlanablagojevic <svetlana.sege@gmail.com>
2016-12-20 10:14:40 +0100

    Delete VotingException.java

--- techdegree-javaweb-unit-testing-0.0.3/src/main/java/com/teamtreehouse/techdegree/overboard/exc/VotingException.java
+++
@@ -1,9 +0,0 @@
-package com.teamtreehouse.techdegree.overboard.exc;
-
-public class VotingException extends RuntimeException {
-
-    public VotingException(String message) {
-
-        super(message);
-    }
-}
